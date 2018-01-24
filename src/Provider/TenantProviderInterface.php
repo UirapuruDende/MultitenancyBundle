32b6a8332810c022d1fc1a5c7fef302b1e3e87cd
Grzegorz 'Uirapuru' Kaszuba <uirapuruadg@gmail.com>
2018-01-24 20:40:57 +0100

    up

--- src/Provider/TenantProviderInterface.php
+++ src/Provider/TenantProviderInterface.php
@@ -5,7 +5,7 @@ use Dende\MultitenancyBundle\DTO\Tenant;
 
 interface TenantProviderInterface
 {
-    public function getTenant() : Tenant;
+    public function getTenant() : ?Tenant;
 
     public function setTenantId(string $id) : void;
 }
